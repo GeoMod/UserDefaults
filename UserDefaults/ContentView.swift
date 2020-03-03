40518f0f2c2fa42ba8de9d512836486062235850
Dan O'Leary <olearydaniel@me.com>
2020-03-03 08:27:21 -0600

    Initial Commit

---
+++ UserDefaults/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserDefaults
+//
+//  Created by Daniel O'Leary on 3/3/20.
+//  Copyright © 2020 Impulse Coupled Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
